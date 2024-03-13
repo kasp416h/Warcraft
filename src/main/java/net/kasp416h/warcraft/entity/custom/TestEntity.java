d955e43ecaf57a483b1a9a7389a3fe3ab3446e3e
Kasper Høg <102671051+kasp416h@users.noreply.github.com>
2024-03-13 14:59:47 +0000

    tesentity test attributes

--- src/main/java/net/kasp416h/warcraft/entity/custom/TestEntity.java
+++ src/main/java/net/kasp416h/warcraft/entity/custom/TestEntity.java
@@ -1,5 +1,16 @@
 package net.kasp416h.warcraft.entity.custom;
 
-public class TestEntity {
-    
+import net.minecraft.world.entity.mobentity.MobEntity;
+
+public class TestEntity extends MobEntity {
+    public TestEntity(EntityType<? extends MobEntity> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+    public static AttributeSupplier.Builder createAttributes() {
+        return MobEntity.createLivingAttributes()
+            .add(Attributes.MAX_HEALTH, 200)
+            .add(Attributes.MOVEMENT_SPEED, 0.25D)
+            .add(Attributes.ARMOR_TOUGHNESS, 0.1f);
+    }
 }
