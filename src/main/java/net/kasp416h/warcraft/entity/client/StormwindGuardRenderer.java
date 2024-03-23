c7f7c042cf91a1464a522a1a2425646da0708f69
Kasper Høg <mailkasperhog@gmail.com>
2024-03-23 16:18:06 +0100

    using PLAYER model

--- src/main/java/net/kasp416h/warcraft/entity/client/StormwindGuardRenderer.java
+++ src/main/java/net/kasp416h/warcraft/entity/client/StormwindGuardRenderer.java
@@ -1,28 +1,20 @@
 package net.kasp416h.warcraft.entity.client;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-
 import net.kasp416h.warcraft.Warcraft;
+import net.minecraft.client.model.geom.ModelLayers;
 import net.kasp416h.warcraft.entity.custom.StormwindGuardEntity;
-import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
 public class StormwindGuardRenderer
-        extends MobRenderer<StormwindGuardEntity, StormwindGuardModel<StormwindGuardEntity>> {
+        extends HumanoidMobRenderer<StormwindGuardEntity, StormwindGuardModel<StormwindGuardEntity>> {
     public StormwindGuardRenderer(EntityRendererProvider.Context pContext) {
-        super(pContext, new StormwindGuardModel<>(pContext.bakeLayer(ModModelLayers.STORMWIND_GUARD_LAYER)), 0.5f);
+        super(pContext, new StormwindGuardModel<>(pContext.bakeLayer(ModelLayers.PLAYER)), 0.5f);
     }
 
     @Override
     public ResourceLocation getTextureLocation(StormwindGuardEntity pEntity) {
         return new ResourceLocation(Warcraft.MODID, "textures/entity/stormwind_guard.png");
-    }
-
-    @Override
-    public void render(StormwindGuardEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pMatrixStack,
-            MultiBufferSource pBuffer, int pPackedLight) {
-        super.render(pEntity, pEntityYaw, pPartialTicks, pMatrixStack, pBuffer, pPackedLight);
     }
 }
